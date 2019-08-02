8bd2ee55942a0d4b983bdf677a34d63fa3568d3a
badubizzle <badu.boahen@gmail.com>
2019-08-02 15:12:18 +0000

    Add trie implementation Updates tests

--- test/data_structures_test.exs
+++ test/data_structures_test.exs
@@ -3,4 +3,58 @@ defmodule DataStructuresTest do
   doctest LinkedList
   doctest BinaryTree
   doctest BinaryNode
+
+  doctest TrieNode
+  doctest Trie
+
+
+  describe "Trie" do
+    test "Insert word into trie" do
+      word = "hello"
+      trie = 
+      Trie.new()
+      |>Trie.insert(word)      
+
+      assert Trie.get_words(trie) == ["hello"]
+    end
+
+    test "Insert word and find in trie" do
+      word = "hello"
+      trie = 
+      Trie.new()
+      |> Trie.insert(word)      
+      assert Trie.search(trie,word) == true
+      assert Trie.search(trie,"hell") == false
+
+      trie = Trie.insert(trie, "hell")
+      assert Trie.search(trie,"hell") == true
+    end
+
+    test "get all words" do
+      word = "hello"
+      trie = Trie.new()
+      trie = Trie.insert(trie,word)      
+      assert Trie.search(trie,word) == true
+      assert Trie.search(trie,"hell") == false
+
+      trie = Trie.insert(trie, "hell")
+      assert Trie.search(trie,"hell") == true
+
+      trie = Trie.insert(trie, "ball")
+
+      trie = 
+      trie
+      |> Trie.insert("hi")
+      |> Trie.insert("hey")
+      |> Trie.insert("he")
+      |> Trie.insert("home")
+      |> Trie.insert("house")
+      |> Trie.insert("jay")
+      |> Trie.insert("joe")
+      
+      assert Trie.prefix(trie,"j")==["joe", "jay"]
+      assert Trie.prefix(trie,"ho")==["house", "home"]
+      
+    end
+  end
 end
