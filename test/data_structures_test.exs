99e0ea560bda0ed6dd1ca188fd81a33a8aad421d
badubizzle <badu.boahen@gmail.com>
2019-08-05 22:34:21 +0000

    Updated tests

--- test/data_structures_test.exs
+++ test/data_structures_test.exs
@@ -6,39 +6,44 @@ defmodule DataStructuresTest do
 
   doctest TrieNode
   doctest Trie
-
+  doctest SelectionSort
+  doctest MergeSort
+  doctest InsertionSort
 
   describe "Trie" do
     test "Insert word into trie" do
       word = "hello"
+
       trie = 
       Trie.new()
-      |>Trie.insert(word)      
+        |> Trie.insert(word)
 
       assert Trie.get_words(trie) == ["hello"]
     end
 
     test "Insert word and find in trie" do
       word = "hello"
+
       trie = 
       Trie.new()
       |> Trie.insert(word)      
-      assert Trie.search(trie,word) == true
-      assert Trie.search(trie,"hell") == false
+
+      assert Trie.search(trie, word) == true
+      assert Trie.search(trie, "hell") == false
 
       trie = Trie.insert(trie, "hell")
-      assert Trie.search(trie,"hell") == true
+      assert Trie.search(trie, "hell") == true
     end
 
     test "get all words" do
       word = "hello"
       trie = Trie.new()
-      trie = Trie.insert(trie,word)      
-      assert Trie.search(trie,word) == true
-      assert Trie.search(trie,"hell") == false
+      trie = Trie.insert(trie, word)
+      assert Trie.search(trie, word) == true
+      assert Trie.search(trie, "hell") == false
 
       trie = Trie.insert(trie, "hell")
-      assert Trie.search(trie,"hell") == true
+      assert Trie.search(trie, "hell") == true
 
       trie = Trie.insert(trie, "ball")
 
@@ -52,9 +57,8 @@ defmodule DataStructuresTest do
       |> Trie.insert("jay")
       |> Trie.insert("joe")
       
-      assert Trie.prefix(trie,"j")==["joe", "jay"]
-      assert Trie.prefix(trie,"ho")==["house", "home"]
-      
+      assert Trie.prefix(trie, "j") == ["joe", "jay"]
+      assert Trie.prefix(trie, "ho") == ["house", "home"]
     end
   end
 end
